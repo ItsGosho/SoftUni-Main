f7a3ced06a210e29a0700c55e695acd0453f9c59
Georgi Peev <itsgosho2@gmail.com>
2018-02-05 18:41:09 +0200

    .
    up Java Calculator

---
+++ #2 Tech Module/#2.Software Technologies/#13.Java Calculator/src/main/java/com/softuni/entity/Calculator.java
@@ -0,0 +1,62 @@
+package com.softuni.entity;
+
+public class Calculator {
+    private double leftOperand;
+    private double rightOperand;
+    private String operator;
+
+    public double getLeftOperand() {
+        return leftOperand;
+    }
+
+    public void setLeftOperand(double leftOperand) {
+        this.leftOperand = leftOperand;
+    }
+
+    public double getRightOperand() {
+        return rightOperand;
+    }
+
+    public void setRightOperand(double rightOperand) {
+        this.rightOperand = rightOperand;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+    public Calculator(double leftOperand, double rightOperand, String operator) {
+        this.leftOperand = leftOperand;
+        this.rightOperand = rightOperand;
+        this.operator = operator;
+    }
+
+    public double calculateResult() {
+        double result = 0;
+        switch (operator) {
+            case "+":
+                result = this.leftOperand + this.rightOperand;
+                break;
+            case "-":
+                result = this.leftOperand - this.rightOperand;
+                break;
+            case "*":
+                result = this.leftOperand * this.rightOperand;
+                break;
+            case "/":
+                result = this.leftOperand / this.rightOperand;
+                break;
+            case "MathPow":
+                result=Math.pow(this.leftOperand,this.rightOperand);
+                break;
+            default:
+                result = 0;
+
+        }
+        return result;
+    }
+}
