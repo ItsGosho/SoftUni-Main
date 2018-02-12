77550a580442f58855ec61352c42fa5ecc6d8e14
Georgi Peev <itsgosho2@gmail.com>
2018-02-12 20:30:31 +0200

    .
    Up C# Blog and Rename the previous folder to Calculator because there was 2 exercises but the 2nd is ugly :/

---
+++ #2 Tech Module/#2.Software Technologies/#17.C# Blog/Blog/Migrations/Configuration.cs
@@ -0,0 +1,33 @@
+namespace Blog.Migrations
+{
+    using Blog.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    public sealed class Configuration : DbMigrationsConfiguration<BlogDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
+        }
+
+        protected override void Seed(BlogDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
