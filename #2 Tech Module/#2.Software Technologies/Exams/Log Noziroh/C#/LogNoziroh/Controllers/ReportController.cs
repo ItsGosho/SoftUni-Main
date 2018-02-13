a517d261cee8b91531d4697b4223bf5992bf703f
Georgi Peev <itsgosho2@gmail.com>
2018-02-13 18:55:15 +0200

    .
    C# softtech exam up LogNoziroh

---
+++ #2 Tech Module/#2.Software Technologies/Exams/Log Noziroh/C#/LogNoziroh/Controllers/ReportController.cs
@@ -0,0 +1,93 @@
+﻿namespace LogNoziroh.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Mvc;
+    using Models;
+
+    [ValidateInput(false)]
+    public class ReportController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        public ActionResult Index()
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var reports = db.Reports.ToList();
+                return View(reports);
+            }
+        }
+
+        [HttpGet]
+        [Route("details/{id}")]
+        public ActionResult Details(int id)
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+                if (report != null)
+                {
+                    return View(report);
+                }
+            }
+            return Redirect("/");
+        }
+
+        [HttpGet]
+        [Route("create")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Report report)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var db = new LogNozirohDbContext())
+                {
+                    db.Reports.Add(report);
+                    db.SaveChanges();
+                    return Redirect("/");
+                }
+            }
+            return View();
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public ActionResult Delete(int id)
+        {
+            using(var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+                if (report != null)
+                {
+                    return View(report);
+                }
+            }
+            return Redirect("/");  
+        }
+
+        [HttpPost]
+        [Route("delete/{id}")] 
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int id, Report reportModel)
+        {
+            using (var db = new LogNozirohDbContext())
+            {
+                var report = db.Reports.Find(id);
+                if (report != null)
+                {
+                    db.Reports.Remove(report);
+                    db.SaveChanges();
+                }
+            }
+            return Redirect("/");
+        }
+    }
+}
