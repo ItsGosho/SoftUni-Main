cc1647d1b1b48ebbd71c7250b5e04977aa06f1c5
Georgi Peev <itsgosho2@gmail.com>
2018-04-18 21:06:16 +0300

    .
    remove old exam to replace it with new from Software Technologies

--- #2 Tech Module/#2.Software Technologies/Exams/Project-Raider/Java/src/main/java/projectrider/repository/ProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package projectrider.repository;
-
-import projectrider.entity.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectRepository extends JpaRepository<Project, Integer> {
-}
