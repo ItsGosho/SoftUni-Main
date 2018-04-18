f2a07645500291b604cd987b938fd05ddd60235c
Georgi Peev <itsgosho2@gmail.com>
2018-04-18 21:06:36 +0300

    .
    remove old exam to replace it with new from Software Technologies

--- #2 Tech Module/#2.Software Technologies/Exams/Anime List/Java/src/main/java/animelist/repository/AnimeRepository.java
+++
@@ -1,7 +0,0 @@
-package animelist.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import animelist.entity.Anime;
-
-public interface AnimeRepository extends JpaRepository<Anime, Integer> {
-}
