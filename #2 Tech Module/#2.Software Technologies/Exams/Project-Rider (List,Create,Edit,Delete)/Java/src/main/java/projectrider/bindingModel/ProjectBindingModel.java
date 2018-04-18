f0cc6f1a50b10d7d1b362cc99ee4ac5a3e2e822f
Georgi Peev <itsgosho2@gmail.com>
2018-04-18 21:09:50 +0300

    .
    added Software Technologies->Project Rider->Java

---
+++ #2 Tech Module/#2.Software Technologies/Exams/Project-Rider (List,Create,Edit,Delete)/Java/src/main/java/projectrider/bindingModel/ProjectBindingModel.java
@@ -0,0 +1,31 @@
+package projectrider.bindingModel;
+
+public class ProjectBindingModel {
+
+    private String title;
+    private String description;
+    private int budget;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String comments) {
+        this.description = comments;
+    }
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+}
---
+++ #2 Tech Module/#2.Software Technologies/Exams/Project-Rider (List,Create,Edit,Delete)/Java/src/main/java/projectrider/entity/Project.java
@@ -0,0 +1,52 @@
+package projectrider.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private int budget;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String comments) {
+        this.description = comments;
+    }
+
+    public int getBudget() {
+        return budget;
+    }
+
+    public void setBudget(int budget) {
+        this.budget = budget;
+    }
+}
