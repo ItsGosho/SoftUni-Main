c9161ce87bd39ee2946e14fc36b4a6368429ce28
Georgi Peev <itsgosho2@gmail.com>
2018-04-21 18:05:10 +0300

    .
    Software Technologies->Exams->Log Noziroh ->doc

---
+++ #2 Tech Module/#2.Software Technologies/Exams/Log Noziroh/JavaScript-Skeleton/models/Report.js
@@ -0,0 +1,22 @@
+const Sequelize = require('sequelize');
+
+module.exports = function (sequelize) {
+    let Report = sequelize.define('Report',{
+        status:{
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+        message:{
+            type: Sequelize.TEXT,
+            allowNull: false,
+        },
+        origin:{
+            type: Sequelize.TEXT,
+            allowNull: false,
+        }
+    },{
+        timestamps:false
+    });
+
+    return Report;
+};
