dafc4a607a76432182d51328452ca3dbbe05b290
Georgi Peev <itsgosho2@gmail.com>
2018-04-21 18:02:43 +0300

    .
    Software Technologies->Exams->Shopping List->doc

---
+++ #2 Tech Module/#2.Software Technologies/Exams/Shopping List(Cannot get edit)/CSharp/ShoppingList/Controllers/ProductController.cs
@@ -0,0 +1,62 @@
+﻿namespace ShoppingList.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Models;
+
+    public class ProductController : Controller
+    {
+        private readonly ShoppingListDbContext dbContext;
+
+        public ProductController(ShoppingListDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            var products = dbContext.Products.ToList();
+            return View(products);
+        }
+
+        [HttpGet]
+        [Route("/create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/create")]
+        public IActionResult Create(Product product)
+        {
+            dbContext.Products.Add(product);
+            dbContext.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(int? id)
+        {
+            var product = dbContext
+                .Products
+                .Where(p => p.Id == id)
+                .FirstOrDefault();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        [Route("/edit/{id}")]
+        public IActionResult Edit(Product product)
+        {
+            dbContext.Products.Update(product);
+            dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
