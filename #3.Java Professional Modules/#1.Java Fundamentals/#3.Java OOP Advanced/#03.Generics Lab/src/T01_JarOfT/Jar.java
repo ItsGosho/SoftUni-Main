44eba800fb6f3bff4d2ee1ad4c4f2d76b8a526b0
Georgi Peev <itsgosho2@gmail.com>
2018-04-27 15:26:23 +0300

    .....
    Java OOP Advanced ->Generics Lab-> /TASK/ jar of T

---
+++ #3.Java Professional Modules/#1.Java Fundamentals/#3.Java OOP Advanced/#03.Generics Lab/src/T01_JarOfT/Jar.java
@@ -0,0 +1,18 @@
+package T01_JarOfT;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Jar<T> {
+    private Deque<T> elements;
+
+    public Jar(){
+        this.elements=new ArrayDeque<>();
+    }
+    public void add(T element){
+        this.elements.push(element);
+    }
+    public T remove(){
+        return this.elements.pop();
+    }
+}
