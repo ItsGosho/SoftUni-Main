f89aaca692cf15e081a3aaa8ebb0c938209867a2
Georgi Peev <itsgosho2@gmail.com>
2018-04-27 15:29:56 +0300

    .....
    Java OOP Advanced ->Generics Exercises-> /TASK/ custom list

---
+++ #3.Java Professional Modules/#1.Java Fundamentals/#3.Java OOP Advanced/#04.Generics Exercises/src/T07_customList/CustomList.java
@@ -0,0 +1,65 @@
+package T07_customList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomList<T> {
+    private List<T> list;
+
+    public CustomList() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.list.add(element);
+    }
+    public void remove(Integer index){
+        this.list.remove(index);
+    }
+    public boolean contains(T element){
+        if(this.list.contains(element)){
+            return true;
+        }
+        return false;
+    }
+    public void swap(Integer index1,Integer index2) {
+        T element2=this.list.get(index2);
+        this.list.set(index2,this.list.get(index1));
+        this.list.set(index1,element2);
+    }
+    public <T extends Comparable<T>> Integer greater(T element){
+        int count=0;
+        for (int i = 0; i < this.list.size(); i++) {
+            T currentElement= (T) this.list.get(i);
+            if(element.compareTo(currentElement)<0){
+                count++;
+            }
+        }
+        return count;
+    }
+    public <T extends Comparable<T>> T min(){
+        T element= (T) this.list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            T currElement= (T) this.list.get(i);
+            if(element.compareTo(currElement)>0){
+                element=currElement;
+            }
+        }
+        return element;
+    }
+    public <T extends Comparable<T>> T max(){
+        T element= (T) this.list.get(0);
+        for (int i = 1; i < list.size(); i++) {
+            T currElement= (T) this.list.get(i);
+            if(element.compareTo(currElement)<0){
+                element=currElement;
+            }
+        }
+        return element;
+    }
+    public void print(){
+        for (int i = 0; i < this.list.size(); i++) {
+            System.out.println(this.list.get(i));
+        }
+    }
+}
---
+++ #3.Java Professional Modules/#1.Java Fundamentals/#3.Java OOP Advanced/#04.Generics Exercises/src/T07_customList/Main.java
@@ -0,0 +1,44 @@
+package T07_customList;
+
+import java.util.Scanner;
+
+public class Main {
+    //Gosho------------------------------>
+    public static void main(String[] args) {
+        Scanner skener = new Scanner(System.in);
+        CustomList<String> customList=new CustomList<>();
+        while(true){
+            String line=skener.nextLine();
+            if(line.equals("END")){
+                 break;
+            }
+            String[] vhod=line.split(" ");
+            switch(vhod[0]){
+                case "Add":
+                    customList.add(vhod[1]);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(vhod[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(vhod[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(vhod[1]),Integer.parseInt(vhod[2]));
+                    break;
+                case "Greater":
+                    System.out.println(customList.greater(vhod[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.max());
+                    break;
+                case "Min":
+                    System.out.println(customList.min());
+                    break;
+                case "Print":
+                    customList.print();
+                    break;
+            }
+        }
+    }
+}
