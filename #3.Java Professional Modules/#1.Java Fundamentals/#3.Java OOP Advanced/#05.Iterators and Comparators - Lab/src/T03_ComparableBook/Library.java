a968920baea6d0c325f268cbecbf2d1327505e2a
Georgi Peev <itsgosho2@gmail.com>
2018-04-27 19:51:38 +0300

    .....
    Java OOP Advanced->Iterators and Comparators->/TASK/ book

---
+++ #3.Java Professional Modules/#1.Java Fundamentals/#3.Java OOP Advanced/#05.Iterators and Comparators - Lab/src/T03_ComparableBook/Library.java
@@ -0,0 +1,30 @@
+package T03_ComparableBook;
+
+import java.util.Iterator;
+
+public class Library implements Iterable<Book> {
+
+    private Book[] books;
+
+    public Library(Book... books) {
+        this.books = books;
+    }
+
+    @Override
+    public Iterator<Book> iterator() {
+        return new LibIterator();
+    }
+
+    private class LibIterator implements Iterator<Book>{
+         private int counter;
+        @Override
+        public boolean hasNext() {
+            return counter<books.length;
+        }
+
+        @Override
+        public Book next() {
+            return books[counter++];
+        }
+    }
+}
