354a310139c0c57ca48d62aff7c232d52ceae813
Georgi Peev <itsgosho2@gmail.com>
2018-04-30 17:54:00 +0300

    .....
    Java OOP Advanced -> Enumerations and Annotations - Lab->/TASK/ create annotation

---
+++ #3.Java Professional Modules/#1.Java Fundamentals/#3.Java OOP Advanced/#07.Enumerations and Annotations - Lab/src/T04_CreateAnnotation/Main.java
@@ -0,0 +1,10 @@
+package T04_CreateAnnotation;
+
+import java.util.Scanner;
+
+public class Main {
+    //Gosho------------------------------>
+    public static void main(String[] args) {
+        Scanner skener = new Scanner(System.in);
+    }
+}
