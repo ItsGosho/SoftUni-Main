5d681e5304c61f34de6e6b0dcec59d0e316ce037
Georgi Peev <itsgosho2@gmail.com>
2018-06-17 12:38:46 +0300

    up
    Java DB Fundamentals ->
    Databases Frameworks - Hibernate & Spring Data  ->
    DB Apps Introduction Exercises->
    /task/ increase minion age

---
+++ #3.Java Professional Modules/#2.Java DB Fundamentals/Databases Frameworks - Hibernate & Spring Data/#02.DB Apps Introduction Exercises/src/T07_Increase_Minions_Age.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class T07_Increase_Minions_Age {
+    //Gosho------------------------------>
+    public static void main(String[] args) {
+        Scanner skener = new Scanner(System.in);
+
+
+    }
+}
