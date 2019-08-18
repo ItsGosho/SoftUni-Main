61ff441e064d120dbfde4d16d3c001faed75e64d
Georgi Peev <itsgosho2@gmail.com>
2019-08-18 14:54:29 +0300

    update .gitignore & constants
    constants for URLs

---
+++ #3.Java Professional Modules/#5.JS Web/#01.ExpressJS/#06.Persistence - Files and MongoDB -Exercises/MemeDB/constants/url.constants.js
@@ -0,0 +1,13 @@
+const URLs = {
+
+   BASE: {
+      HOME: '/'
+   },
+   MEME: {
+      VIEW_ALL: '/meme/view/all',
+      ADD: '/meme/add',
+      DETAILS: '/meme/details',
+   }
+};
+
+module.exports = URLs;
