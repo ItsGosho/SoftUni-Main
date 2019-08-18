e146b9899783a4e2395159d826eddea2cff860d1
Georgi Peev <itsgosho2@gmail.com>
2019-08-18 22:14:21 +0300

    lowercase for tag schema

--- #3.Java Professional Modules/#5.JS Web/#01.ExpressJS/#06.Persistence - Files and MongoDB -Exercises/MongoPlayground/models/TagSchema.js
+++ #3.Java Professional Modules/#5.JS Web/#01.ExpressJS/#06.Persistence - Files and MongoDB -Exercises/MongoPlayground/models/TagSchema.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const tagSchema = new Schema({
     name: {
         type: Schema.Types.String,
-        require: true
+        require: true,
+        lowercase: true
     },
     images: {
         type: Schema.Types.Array,
