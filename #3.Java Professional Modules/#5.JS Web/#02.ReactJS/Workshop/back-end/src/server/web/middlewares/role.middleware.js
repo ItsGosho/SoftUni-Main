fc88c146c980fb8ad5eb4b0e4c324e255b2a4ad5
itsgosho <itsgosho2@gmail.com>
2019-09-23 20:17:17 +0300

    Fixed Role Middleware: 1.It was using the old way of getting the current user role ,now it will via he RoleHelper

--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/middlewares/role.middleware.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/middlewares/role.middleware.js
@@ -1,18 +1,19 @@
 import RestResponseHelper from "../helpers/rest.response.helper";
 import JWTServices from "../../services/jwt.token.services";
+import RoleHelper from "../../helpers/role.helper";
 
 /*Note that before this middleware is executed a user must be attached to the request somehow*/
 
 const RoleMiddleware = (requiredRole) => {
     return async (request, response, next) => {
-        /* let user = request.user;
-         let role = await RoleServices.findRoleByUserId(user.id);
+        let user = request.user;
+        let role = RoleHelper.getHighestRoleFrom(user.roles);
 
-         if (role.name.toLowerCase() === requiredRole.toLowerCase()) {
-             next();
-             return;
-         }
- */
+        if (role.toLowerCase() === requiredRole.toLowerCase()) {
+            next();
+            return;
+        }
+
         RestResponseHelper.respondErrorMessage(response, `You must be ${requiredRole} to access this route!`)
     }
 };
