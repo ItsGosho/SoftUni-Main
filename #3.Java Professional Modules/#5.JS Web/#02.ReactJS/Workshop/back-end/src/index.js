2efa60b22f305d0ecc030c0bcc195bf257aee4a8
itsgosho <itsgosho2@gmail.com>
2019-09-24 22:08:18 +0300

    Removed the way of passing the middlewares into route as array ,because of hard readability

--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/index.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/index.js
@@ -4,6 +4,7 @@ import ExpressConfiguration from './server/configurations/express.configuration'
 import DatabaseConfiguration from './server/configurations/database.configuration';
 import Properties from "./server/configurations/properties";
 import {ServerLogging} from "./server/constants/server/server.logging.constants";
+import UserRequestValidators from "./server/web/validators/user.request.validators";
 
 let app = Express();
 
@@ -13,4 +14,7 @@ DatabaseConfiguration();
 app.listen(Properties.server.port, () => {
     console.log(ServerLogging.SERVER_STARTED);
 });
+
+let func = UserRequestValidators.usernameNotPresent('username');
+console.log(UserRequestValidators.usernameNotPresent('username'));
 
--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/configurations/middlewares.configuration.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/configurations/middlewares.configuration.js
@@ -6,6 +6,7 @@ import LoggedInMiddleware from "../web/middlewares/logged.in.middleware";
 import ValidationResponseMiddleware from "../web/middlewares/validation.response.middleware";
 import LoggedOutMiddleware from "../web/middlewares/logged.out.middleware";
 import {UserRoutingURLs} from "../constants/web/routing.urls.constants";
+
 const Router = Express.Router();
 
 Router.all('*', UserAttacherMiddleware);
@@ -13,24 +14,23 @@ Router.all('*', UserAttacherMiddleware);
 
 /*Authentication*/
 Router.post(UserRoutingURLs.LOGIN,
-    [
-        LoggedOutMiddleware,
-        Body()
-            .custom(UserRequestValidators.credentialsValid('username','password')).bail(),
-        ValidationResponseMiddleware
-    ]);
+    LoggedOutMiddleware,
+    Body()
+        .custom(UserRequestValidators.credentialsValid('username', 'password')).bail(),
+    ValidationResponseMiddleware
+);
 
 Router.post(UserRoutingURLs.REGISTER,
-    [LoggedOutMiddleware,
-        Body()
-            .custom(UserRequestValidators.usernameNotPresent('username')).bail()
-            .custom(UserRequestValidators.emailNotPresent('email')).bail()
-            .custom(UserRequestValidators.passwordsMustMatch('password', 'confirmPassword')).bail()
-            .custom(UserRequestValidators.email('email')).bail(),
-        ValidationResponseMiddleware
-    ]);
+    LoggedOutMiddleware,
+    Body()
+        .custom(UserRequestValidators.usernameNotPresent('username')).bail()
+        .custom(UserRequestValidators.emailNotPresent('email')).bail()
+        .custom(UserRequestValidators.passwordsMustMatch('password', 'confirmPassword')).bail()
+        .custom(UserRequestValidators.email('email')).bail(),
+    ValidationResponseMiddleware
+);
 
-Router.post(UserRoutingURLs.LOGOUT,LoggedInMiddleware);
+Router.post(UserRoutingURLs.LOGOUT, LoggedInMiddleware);
 
 
 export default Router;
