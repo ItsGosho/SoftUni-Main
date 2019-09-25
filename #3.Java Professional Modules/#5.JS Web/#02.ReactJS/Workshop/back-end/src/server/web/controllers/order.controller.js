6fdeaedf730488ef9c04fff02a31b40a83d2c19b
itsgosho <itsgosho2@gmail.com>
2019-09-25 23:26:21 +0300

    Order Controller`s routing handlers`s skeletons have been placed ,again with detailed information what needs to happen at this route

--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/controllers/order.controller.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/controllers/order.controller.js
@@ -22,11 +22,26 @@ Router.post(OrderRoutingURLs.MY,(request,response)=>{
     * */
 });
 
-Router.post(OrderRoutingURLs.PENDING,(request,response)=>{
+Router.post(OrderRoutingURLs.ALL,(request,response)=>{
     /*
     * TODO:
     *  1.Дали user-a е логнат
-    *  2.Взимам на user-a ID-то и му намирам всички order-и
-    *  3.Respond-вам successful с "Orders have been fetched!" и order-ите
+    *  2.Дали user-a е [ADMIN]
+    *  3.Взимам QUERY PARAM за сега с status (request.query.status)
+    *  4.Взимам всички order-и,които имат този статус
+    *  5.Respond-вам successful с "Orders have been fetched!" и order-ите
+    * */
+});
+
+Router.post(OrderRoutingURLs.APPROVE,(request,response)=>{
+    /*
+    * TODO:
+    *  1.Дали user-a е логнат
+    *  2.Дали user-a е [ADMIN]
+    *  3.Дали съществува такъв order
+    *  4.Взимам ID-то на order-a (request.params.id)
+    *  5.Сменям му статуса на Approve
+    *  6.Save-aм го
+    *  6.Respond-вам successful с "Orders has been approved!"
     * */
 });
