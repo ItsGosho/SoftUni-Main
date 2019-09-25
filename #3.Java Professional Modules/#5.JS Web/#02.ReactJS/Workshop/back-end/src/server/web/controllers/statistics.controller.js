b05ea304a39a411b7205a77bb4fc10b408fc7cb2
itsgosho <itsgosho2@gmail.com>
2019-09-25 22:46:20 +0300

    The three statistic controllers`s routes are present in the controller as skeleton ,with TODO information in them

--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/controllers/statistics.controller.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/back-end/src/server/web/controllers/statistics.controller.js
@@ -1,5 +1,34 @@
 import Express from "express";
+import {StatisticsRoutingURLs} from "../../constants/web/routing.urls.constants";
 
 const Router = Express.Router();
 
-Router.get();
+Router.get(StatisticsRoutingURLs.TOTAL_USERS, (request,response) => {
+    /*
+    * TODO:
+    *  1.Check if there is logged in user
+    *  2.Check if the user role is [ADMIN]
+    *  3.Call the user services to find the total number of users in the database
+    *  4.Return their count
+    * */
+});
+
+Router.get(StatisticsRoutingURLs.TOTAL_BOOKS, (request,response) => {
+    /*
+    * TODO:
+    *  1.Check if there is logged in user
+    *  2.Check if the user role is [ADMIN]
+    *  3.Call the books services to find the total number of books in the database
+    *  4.Return their count
+    * */
+});
+
+Router.get(StatisticsRoutingURLs.TOTAL_ORDERS, (request,response) => {
+    /*
+    * TODO:
+    *  1.Check if there is logged in user
+    *  2.Check if the user role is [ADMIN]
+    *  3.Call the orders services to find the total number of orders in the database
+    *  4.Return their count
+    * */
+});
