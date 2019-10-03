0bd756647f13a91e0d743a3ab4b6b256f58665ae
itsgosho <itsgosho2@gmail.com>
2019-10-03 08:49:49 +0300

    Separate the Books map logic ,from the TopRatedBooks into BookIterator into separate Component ,which reused into the Store

---
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/front-end/src/components/book/BookIterator.js
@@ -0,0 +1,44 @@
+import React,{Component} from 'react';
+import RoutingURLs from "../../constants/routing.url.constants";
+import bookFetchReduxHoc from "../../redux/book/hocs/book.fetch.hoc";
+
+class BookIterator extends Component {
+
+    componentDidMount() {
+        this.props.fetchAll();
+    }
+
+    render() {
+        let books = [...this.props.redux.books];
+        let topRated = books.sort((x1, x2) => x2.likes.length - x1.likes.length).splice(0, 3);
+
+        return (
+            <div className="card-deck space-top">
+
+                {topRated.map((movie, index) => {
+                    let {_id, title, description, image} = movie;
+                    let detailsUrl = RoutingURLs.BOOK.DETAILS.replace(':id', _id);
+
+                    return (
+                        <div className="card col-4" key={index}>
+                            <img className="card-img-top card-image" src={image} alt={title}/>
+                            <div className="card-body">
+                                <h5 className="card-title">{title}</h5>
+                                <p className="card-text">{description}</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted"/>
+                                <a type="button" className="btn btn-primary float-right btn-sm"
+                                   href={detailsUrl}>Details</a>
+                                <button type="button" className="btn btn-warning float-right btn-sm">Order</button>
+                            </div>
+                        </div>
+                    );
+                })};
+
+            </div>
+        );
+    }
+}
+
+export default bookFetchReduxHoc(BookIterator);
--- #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/front-end/src/components/store/Store.js
+++ #3.Java Professional Modules/#5.JS Web/#02.ReactJS/Workshop/front-end/src/components/store/Store.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
+import BookIterator from "../book/BookIterator";
 
 
 class Store extends Component {
 
     render() {
         let books = [];
-        console.log(123);
 
         return (
             <div className="container">
@@ -21,40 +21,7 @@ class Store extends Component {
                     </div>
                 </div>
                 <div className="row">
-                    <div className="card-deck space-top">
-                        <div className="card col-4">
-                            <img className="card-img-top card-image"
-                                                         src="https://images-na.ssl-images-amazon.com/images/I/51fonMmNpnL.jpg"
-                                                         alt="Harry Potter"/>
-                            <div className="card-body">
-                                <h5 className="card-title">Harry Potter</h5>
-                                <p className="card-text">Harry Potter and the Philosopher's Stone is a fantasy novel
-                                    written by British author J. K. Rowling.</p>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted"/>
-                                <a type="button" className="btn btn-primary float-right btn-sm" href="/details/5c750355eba4543e800f7ce4">Details</a>
-                                <button type="button" className="btn btn-warning float-right btn-sm">Order</button>
-                            </div>
-                        </div>
-                        <div className="card col-4">
-                            <img className="card-img-top card-image"
-                                                         src="https://images-na.ssl-images-amazon.com/images/I/419M8dJUa-L._SY346_.jpg"
-                                                         alt="What the Wind Knows "/>
-                            <div className="card-body">
-                                <h5 className="card-title">What the Wind Knows </h5>
-                                <p className="card-text">Anne Gallagher grew up enchanted by her grandfather’s stories
-                                    of Ireland. Heartbroken at his death, she travels to his childhood home to spread
-                                    his ashes.</p>
-                            </div>
-                            <div className="card-footer">
-                                <small className="text-muted"/>
-                                <a type="button"
-                                   className="btn btn-primary float-right btn-sm" href="/details/5c75009b6fa7f81b2459134e">Details</a>
-                                <button type="button" className="btn btn-warning float-right btn-sm">Order</button>
-                            </div>
-                        </div>
-                    </div>
+                    <BookIterator/>
                 </div>
                 <div className="row space-top">
                     <div className="col-md-12">
