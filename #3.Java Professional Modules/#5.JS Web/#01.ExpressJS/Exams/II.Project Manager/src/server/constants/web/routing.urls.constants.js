7c99af553998cdc20a8926fe7fd4acf7ac1ff315
itsgosho <itsgosho2@gmail.com>
2019-10-06 15:54:12 +0300

    Created the corresponding RoutingURLs for users,projects and team for the Project Manager`s exam

--- #3.Java Professional Modules/#5.JS Web/#01.ExpressJS/Exams/II.Project Manager/src/server/constants/web/routing.urls.constants.js
+++ #3.Java Professional Modules/#5.JS Web/#01.ExpressJS/Exams/II.Project Manager/src/server/constants/web/routing.urls.constants.js
@@ -1,15 +1,32 @@
 const RoutingURLs = {
-    HOME: '/',
-    USER: {
-        LOGIN: '/login',
-        REGISTER: '/register',
-        LOGOUT: '/logout',
-    },
-};
+  HOME: '/',
 
-const UserRoutingURLs = RoutingURLs.USER;
+  USER: {
+    LOGIN: '/login',
+    REGISTER: '/register',
+    LOGOUT: '/logout',
+    PROFILE: '/user/profile',
+  },
+
+  PROJECT: {
+    CREATE: '/project/create',
+    ALL: '/projects/all',
+  },
+
+  TEAM: {
+    CREATE: '/team/create',
+    ALL: '/teams/all',
+  }
+}
+
+const UserRoutingURLs = RoutingURLs.USER
+const ProjectRoutingURLs = RoutingURLs.PROJECT
+const TeamRoutingURLs = RoutingURLs.TEAM
 
 export {
-    RoutingURLs,
-    UserRoutingURLs
+  RoutingURLs,
+
+  UserRoutingURLs,
+  ProjectRoutingURLs,
+  TeamRoutingURLs,
 }
